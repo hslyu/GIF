f1f83fee6a19c6ce26b17285979f724909437510
hslyu <hslyu4@gmail.com>
2023-06-12 13:30:01 +0900

    Remove test lines

--- models/fcn.py
+++ models/fcn.py
@@ -29,18 +29,3 @@ class FullyConnectedNet(nn.Module):
         for layer in self.layers:
             x = layer(x)
         return x
-
-
-# Set the dimensions and hyperparameters
-input_size = 100
-hidden_size = 200
-output_size = 10
-num_layers = 50
-dropout_prob = 0.2
-
-# Create an instance of the network
-net = FullyConnectedNet(input_size, hidden_size, output_size, num_layers, dropout_prob)
-
-# Print the network architecture
-num_params = sum(p.numel() for p in net.parameters() if p.requires_grad)
-print(f"Network: {type(net)}, Parameters={num_params/1000000:.2f}M")
