62df932d739ac814ad884a7734d16ff8c4ffb489
hslyu <hslyu4@gmail.com>
2023-06-12 13:31:05 +0900

    Check the size of models

---
+++ models/size.py
@@ -0,0 +1,45 @@
+"""Train CIFAR10 with PyTorch."""
+
+from densenet import *
+from dla import *
+from dla_simple import *
+from dpn import *
+from efficientnet import *
+from fcn import *
+from googlenet import *
+from lenet import *
+from mobilenet import *
+from mobilenetv2 import *
+from pnasnet import *
+from preact_resnet import *
+from regnet import *
+from resnet import *
+from resnext import *
+from senet import *
+from shufflenet import *
+from shufflenetv2 import *
+from tinynet import *
+from vgg import *
+
+net_list = [
+    VGG("VGG16"),
+    ResNet18(),
+    PreActResNet18(),
+    GoogLeNet(),
+    DenseNet121(),
+    ResNeXt29_2x64d(),
+    MobileNet(),
+    MobileNetV2(),
+    DPN92(),
+    # ShuffleNetG2(),
+    SENet18(),
+    ShuffleNetV2(1),
+    EfficientNetB0(),
+    RegNetX_200MF(),
+    TinyNet(),
+    SimpleDLA(),
+    DenseNet121(),
+]
+for net in net_list:
+    num_params = sum(p.numel() for p in net.parameters() if p.requires_grad)
+    print(f"Network: {type(net)}, Parameters={num_params/1000000:.2f}M")
