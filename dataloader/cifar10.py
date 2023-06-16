0fc8c526ec0939e6d570c63ed792f4cef58cadfd
hslyu <hslyu4@gmail.com>
2023-06-16 16:15:35 +0900

    Add flatten and one-hot encodings

--- dataloader/cifar10.py
+++ dataloader/cifar10.py
@@ -3,9 +3,16 @@ from torchvision import datasets, transforms
 
 
 class CIFAR10DataLoader:
-    def __init__(self, batch_size=512, num_workers=12):
+    def __init__(self, batch_size=512, num_workers=12, one_hot=False, flatten=False):
         self.batch_size = batch_size
         self.num_workers = num_workers
+        self.one_hot = one_hot
+        self.flatten = flatten
+
+    def one_hot_encoder(self, batch):
+        images, labels = zip(*batch)
+        labels = torch.tensor(labels)
+        return torch.stack(images), torch.nn.functional.one_hot(labels, 10).float()
 
     def get_data_loaders(self):
         # Load the CIFAR-10 dataset
@@ -21,6 +28,7 @@ class CIFAR10DataLoader:
                     transforms.Normalize(
                         (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
                     ),
+                    transforms.Lambda(lambda x: x.view(-1) if self.flatten else x),
                 ]
             ),
         )
@@ -35,6 +43,7 @@ class CIFAR10DataLoader:
                     transforms.Normalize(
                         (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
                     ),
+                    transforms.Lambda(lambda x: x.view(-1) if self.flatten else x),
                 ]
             ),
         )
@@ -45,24 +54,28 @@ class CIFAR10DataLoader:
         split = int(0.8 * num_train)  # 80% for training, 20% for validation
         train_indices, val_indices = indices[:split], indices[split:]
 
+        collate_fn = self.one_hot_encoder if self.one_hot else None
         # Create data loaders for training, validation, and testing datasets
         train_loader = torch.utils.data.DataLoader(
             train_dataset,
             batch_size=self.batch_size,
             sampler=torch.utils.data.sampler.SubsetRandomSampler(train_indices),
             num_workers=self.num_workers,
+            collate_fn=collate_fn,
         )
         val_loader = torch.utils.data.DataLoader(
             train_dataset,
             batch_size=self.batch_size,
             sampler=torch.utils.data.sampler.SubsetRandomSampler(val_indices),
             num_workers=self.num_workers,
+            collate_fn=collate_fn,
         )
         test_loader = torch.utils.data.DataLoader(
             test_dataset,
             batch_size=self.batch_size,
             shuffle=False,
             num_workers=self.num_workers,
+            collate_fn=collate_fn,
         )
 
         return train_loader, val_loader, test_loader
