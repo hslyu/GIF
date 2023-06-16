82ae958d825fb44a5cf1f054acbe15b151fe8fa4
hslyu <hslyu4@gmail.com>
2023-06-16 16:16:23 +0900

    Add new functionality: compute negative eigval rate

--- scripts/negative_eigval_rate.py
+++ scripts/negative_eigval_rate.py
@@ -2,15 +2,16 @@
 # -*- coding: utf-8 -*-
 import argparse
 import os
+import time
 
+import numpy as np
 import torch
 import torch.backends.cudnn as cudnn
-import torch.optim as optim
 from torch import nn
 
-from dataloader import cifar10
-from models import VGG16  # ResNet50, ShuffleNetV2
-from src import utils
+from dataloader import cifar10, mnist
+from models import VGG16, FullyConnectedNet, LeNet  # ResNet50, ShuffleNetV2
+from src import hessians, lanczos, regularization, utils
 
 # from src import hessians
 
@@ -19,130 +20,129 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 
 def get_args():
     parser = argparse.ArgumentParser(description="PyTorch CIFAR10 Training")
-    parser.add_argument("--lr", default=0.1, type=float, help="learning rate")
     parser.add_argument(
-        "--resume", "-r", action="store_true", help="resume from checkpoint"
+        "--network",
+        default="LeNet",
+        choices=["LeNet", "FCN"],
+        type=str,
+        help="Model to be tested",
+    )
+    parser.add_argument("--lr", default=0.1, type=float, help="Learning rate")
+    parser.add_argument(
+        "--num_epoch", default=50, type=int, help="Number of training epochs"
+    )
+    parser.add_argument(
+        "--data",
+        default="CIFAR10",
+        choices=["CIFAR10", "MNIST"],
+        type=str,
+        help="Dataset to be used",
+    )
+    parser.add_argument(
+        "--loss",
+        default="cross_entropy",
+        choices=["cross_entropy", "mse"],
+        type=str,
+        help="Loss function",
+    )
+    parser.add_argument(
+        "--num_eigval", default=300, type=int, help="Number of eigenvalues to compute"
+    )
+    parser.add_argument(
+        "--alpha", default=0.0, type=float, help="Regularization weight"
+    )
+    parser.add_argument(
+        "--resume", "-r", action="store_true", help="Resume from checkpoint"
     )
     return parser.parse_args()
-
-
-# Training
-def train(net, dataloader, optimizer, criterion):
-    net.train()
-    train_loss = 0
-    correct = 0
-    total = 0
-    for batch_idx, (inputs, targets) in enumerate(dataloader):
-        inputs, targets = inputs.to(device), targets.to(device)
-        optimizer.zero_grad()
-        outputs = net(inputs)
-        loss = criterion(outputs, targets)
-        loss.backward()
-        optimizer.step()
-
-        train_loss += loss.item()
-        _, predicted = outputs.max(1)
-        total += targets.size(0)
-        correct += predicted.eq(targets).sum().item()
-
-        utils.progress_bar(
-            batch_idx,
-            len(dataloader),
-            "Loss: %.3f | Acc: %.3f%% (%d/%d)"
-            % (train_loss / (batch_idx + 1), 100.0 * correct / total, correct, total),
-        )
-
-
-def test(net, dataloader, criterion, epoch, best_acc):
-    net.eval()
-    test_loss = 0
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for batch_idx, (inputs, targets) in enumerate(dataloader):
-            inputs, targets = inputs.to(device), targets.to(device)
-            outputs = net(inputs)
-            loss = criterion(outputs, targets)
-
-            test_loss += loss.item()
-            _, predicted = outputs.max(1)
-            total += targets.size(0)
-            correct += predicted.eq(targets).sum().item()
-
-            utils.progress_bar(
-                batch_idx,
-                len(dataloader),
-                "Loss: %.3f | Acc: %.3f%% (%d/%d)"
-                % (
-                    test_loss / (batch_idx + 1),
-                    100.0 * correct / total,
-                    correct,
-                    total,
-                ),
-            )
-
-    # Save checkpoint.
-    acc = 100.0 * correct / total
-    if acc > best_acc:
-        print(f"Saving the model: {acc} > {best_acc}")
-        state = {
-            "net": net.state_dict(),
-            "acc": acc,
-            "epoch": epoch,
-        }
-        if not os.path.isdir("checkpoints"):
-            os.mkdir("checkpoints")
-        torch.save(state, "checkpoints/ckpt.pth")
-        best_acc = acc
 
 
 def main():
     args = get_args()
 
-    batch_size = 512
-    num_workers = 12
-
-    # Data
-    print("==> Preparing data..")
-    data_loader = cifar10.CIFAR10DataLoader(batch_size, num_workers)
-    train_loader, val_loader, test_loader = data_loader.get_data_loaders()
-
+    # Network configuration
     print("==> Building Model..")
-    # net_VGG16 = VGG16().to(device)
-    # net_ResNet50 = ResNet50().to(device)
-    # net_ShuffleNetV2 = ShuffleNetV2().to(device)
-    net = VGG16().to(device)
+    if args.network == "LeNet":
+        net = LeNet().to(device)
+        flatten = False
+    else:
+        net = FullyConnectedNet(28 * 28, 8, 10, 3, 0.1).to(device)
+        flatten = True
+    net_name = net.__class__.__name__
+    print(
+        f"==> Building {net_name} finished. "
+        + f"\n    Number of parameters: {sum(p.numel() for p in net.parameters())}"
+    )
 
     if device == "cuda":
         net = torch.nn.DataParallel(net)
         cudnn.benchmark = True
-        # net_ResNet50 = torch.nn.DataParallel(net_ResNet50)
-        # net_ShuffleNetV2 = torch.nn.DataParallel(net_ShuffleNetV2)
 
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
+    # Load checkpoint.
+    print("==> Resuming from checkpoint..")
+    assert os.path.isfile(
+        f"checkpoints/{net_name}/{args.loss}/ckpt_{args.alpha}.pth"
+    ), "Error: no checkpoint file found!"
+    checkpoint = torch.load(f"checkpoints/{net_name}/{args.loss}/ckpt_{args.alpha}.pth")
+    net.load_state_dict(checkpoint["net"])
+    assert (
+        checkpoint["alpha"] == args.alpha
+    ), "Error: alpha is not equal to checkpoint value!"
+    assert (
+        checkpoint["loss"] == args.loss
+    ), "Error: loss is not equal to checkpoint value!"
 
-    global best_acc
-
-    if args.resume:
-        # Load checkpoint.
-        print("==> Resuming from checkpoint..")
-        assert os.path.isdir("checkpoints"), "Error: no checkpoint directory found!"
-        checkpoint = torch.load("checkpoints/ckpt.pth")
-        net.load_state_dict(checkpoint["net"])
-        best_acc = checkpoint["acc"]
-        start_epoch = checkpoint["epoch"]
+    # Loss configuration
+    if args.loss == "cross_entropy":
+        criterion = regularization.RegularizedLoss(
+            net, nn.CrossEntropyLoss(), args.alpha
+        )
+        one_hot = False
     else:
-        best_acc = 0
-        start_epoch = 0
+        criterion = regularization.RegularizedLoss(net, nn.MSELoss(), args.alpha)
+        one_hot = True
+    print(
+        f"==> Current criterion: {criterion.__class__.__name__} with {args.loss} and alpha={args.alpha}"
+    )
 
-    for epoch in range(start_epoch, start_epoch + 50):
-        print("\nEpoch: %d" % epoch)
-        train(net, train_loader, optimizer, criterion)
-        train(net, val_loader, optimizer, criterion)
-        test(net, test_loader, criterion, epoch, best_acc)
-        scheduler.step()
+    # Data
+    print("==> Preparing data..")
+    batch_size = 512
+    num_workers = 2
+
+    if args.data == "CIFAR10":
+        assert args.network != "FCN", "CIFAR10 is not supported for FCN"
+        data_loader = cifar10.CIFAR10DataLoader(batch_size, num_workers, one_hot)
+    else:
+        data_loader = mnist.MNISTDataLoader(batch_size, num_workers, one_hot, flatten)
+    _, _, test_loader = data_loader.get_data_loaders()
+
+    print("==> Evaluate model loss and hessian..")
+    # One batch of train data
+    data, target = next(iter(test_loader))
+    loss = criterion(net(data.to(device)), target.to(device))
+
+    start = time.time()
+    print("-----------Lanzcos algorithm test------------\n")
+    if device == "cuda":
+        eigvals_lanczos, _ = lanczos.lanczos(
+            loss,
+            net,
+            num_eigenthings=args.num_eigval,
+            tol=0,
+            use_gpu=True,
+        )
+    else:
+        eigvals_lanczos, _ = lanczos.lanczos(
+            loss,
+            net,
+            num_eigenthings=args.num_eigval,
+            tol=0,
+        )
+    print(
+        f"Negative eigval rate by Lanczos algorithms, largest eigvals: {np.sum(eigvals_lanczos < -1e-8)}/ {args.num_eigval} ({np.sum(eigvals_lanczos < -1e-8)/args.num_eigval*100:.3f}%)"
+    )
+    print(f"Computation time: {time.time() - start:.2f}s\n")
 
 
 if __name__ == "__main__":
