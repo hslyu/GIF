1d067855baf9084aae79e6889be3633b3c950ee0
hslyu <hslyu4@gmail.com>
2023-06-19 17:25:24 +0900

    Update recursive train function

--- run/recursive_model_train.sh
+++ run/recursive_model_train.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-for alpha in $(seq -f "%.2f" 0.00 0.01 0.25)
+for path in $(seq 17 100);
 do
-	if [ $# -eq 0 ]; then
-		python scripts/train.py --num_epoch 50 --alpha $alpha
-	elif [ $# -eq 1 ]; then
-		python scripts/train.py --num_epoch 50 --alpha $alpha --loss $1
-	elif [ $# -eq 2 ]; then
-		python scripts/train.py --num_epoch 50 --alpha $alpha --loss $1 --data $2
-	fi
-    message="\"python scripts/train.py --num_epoch 50 --alpha $alpha\" has been finished"
-    echo $message
+	for alpha in $(seq -f "%.2f" 0.00 0.01 0.25)
+	do
+		python scripts/train.py --num_epoch 50 --alpha $alpha --path $path --network $1 --criterion $2 --data $3
+		message="\"python scripts/train.py --num_epoch 50 --alpha $alpha\" has been finished"
+		echo $message
+	done
 done
