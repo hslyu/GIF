7b456934f05b0ff3a1496297fc4490b720af0454
hslyu <hslyu4@gmail.com>
2023-07-04 16:28:43 +0900

    Change filename and path range

--- run/recursive_model_train.sh
+++ run/recursive_model_train.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for path in $(seq 17 100);
+for path in $(seq 0 100);
 do
 	for alpha in $(seq -f "%.2f" 0.00 0.01 0.25)
 	do
-		python scripts/train.py --num_epoch 50 --alpha $alpha --path $path --network $1 --criterion $2 --data $3
+		python scripts/Figure_2-train.py --num_epoch 50 --alpha $alpha --path $path --network $1 --criterion $2 --data $3
 		message="\"python scripts/train.py --num_epoch 50 --alpha $alpha\" has been finished"
 		echo $message
 	done
