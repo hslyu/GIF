cd00c8f01148cfafed836ee022149d43c9d85ede
hslyu <hslyu4@gmail.com>
2023-07-08 16:09:37 +0900

    Add full functionality

--- examples/parameter_selection.py
+++ examples/parameter_selection.py
@@ -2,28 +2,71 @@
 # -*- coding: utf-8 -*-
 import os
 import time
+from dataclasses import dataclass
 
 import torch
 import torch.backends.cudnn as cudnn
 from torch import nn
 
 from dataloader import mnist
-from models import FullyConnectedNet
-from src import lanczos, parameter_selection, regularization
+from models import FullyConnectedNet, TinyNet
+from src import hessians, parameter_selection, utils
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
+def load_net(net, path):
+    assert os.path.isfile(path), "Error: no checkpoint file found!"
+    checkpoint = torch.load(path)
+    net.load_state_dict(checkpoint["net"])
+    train_epoch = checkpoint["epoch"]
+    return net, train_epoch
+
+
+def forward(net, dataloader, criterion):
+    net.train()
+    train_loss = 0
+    # Experimental lines for just doing some tests
+    # count = 0
+    for batch_idx, (inputs, targets) in enumerate(dataloader):
+        inputs, targets = inputs.to(device), targets.to(device)
+        outputs = net(inputs)
+        loss = criterion(outputs, targets)
+        train_loss += loss
+
+        utils.progress_bar(batch_idx, len(dataloader), "")
+        # Experimental lines for just doing some tests
+        # count += 1
+        # if count == 5:
+        #     return train_loss
+
+    train_loss /= len(dataloader)
+    return train_loss
+
+
 def main():
+    torch.manual_seed(0)
     net = FullyConnectedNet(28 * 28, 8, 10, 3, 0.1).to(device)
+    if device == "cuda":
+        net = torch.nn.DataParallel(net)
+        cudnn.benchmark = True
+
+    net, train_epoch = load_net(
+        net,
+        "/home/hslyu/research/PIF/checkpoints/Figure_3/FullyConnectedNet/cross_entropy/ckpt_0.0.pth",
+    )
+
+    # For just doing some exps
+    # net = FullyConnectedNet(28 * 28, 600, 10, 200, 0.1).to(device)
+    # train_epoch = 50
+
+    net.eval()
+    flatten = True
     net_name = net.__class__.__name__
     print(
         f"==> Building {net_name} finished. "
         + f"\n    Number of parameters: {sum(p.numel() for p in net.parameters())}"
     )
-
-    if device == "cuda":
-        cudnn.benchmark = True
 
     criterion = nn.CrossEntropyLoss()
 
@@ -32,16 +75,37 @@ def main():
     batch_size = 512
     num_workers = 2
 
-    data_loader = mnist.MNISTDataLoader(batch_size, num_workers, flatten=True)
-    _, _, test_loader = data_loader.get_data_loaders()
+    data_loader = mnist.MNISTDataLoader(batch_size, num_workers, flatten=flatten)
+    train_loader, val_loader, _ = data_loader.get_data_loaders()
 
+    print("==> Registering hooks..")
     # Make hooks
-    parameter_parser = parameter_selection.TopNActivations(net, 100)
-    parameter_parser.register_hook()
+    net_parser = parameter_selection.TopNActivations(net, 600)
+    net_parser.register_hook()
 
+    print("==> Computing total loss..")
+    total_loss = forward(net, train_loader, criterion)
+    print(total_loss)
+
+    print("==> Computing influence..")
     # One batch of train data
-    data, target = next(iter(test_loader))
-    loss = criterion(net(data.to(device)), target.to(device))
+    for _ in range(train_epoch):
+        for batch_idx, (data, target) in enumerate(train_loader):
+            print("batch_idx: ", batch_idx)
+            net_parser.initialize_neurons()
+            loss = criterion(net(data[0:10].to(device)), target[0:10].to(device))
+
+            idx = target == 8
+            data = data[idx]
+            target = target[idx]
+
+            target_loss = criterion(net(data.to(device)), target.to(device))
+            index_list = net_parser.get_parameters()
+            influence = hessians.partial_influence(
+                index_list, target_loss, total_loss, net
+            )
+
+    net_parser.remove_hooks()
 
 
 if __name__ == "__main__":
