1efb6c1b89409fd7279b3f4fa867b6702069f660
hslyu <hslyu4@gmail.com>
2023-07-10 01:54:19 +0900

    Add Conv2D support and distributed selection

--- src/parameter_selection.py
+++ src/parameter_selection.py
@@ -4,32 +4,89 @@ from torch import nn
 
 
 class TopNActivations(nn.Module):
-    def __init__(self, net, num_neurons):
+    def __init__(self, net, num_choices):
         super(TopNActivations, self).__init__()
-        self.net = net
         self.hooks = []
-        self.num_neurons = num_neurons
-        self.activated_neurons = np.zeros(num_neurons, dtype="int16")
+        self.num_choices = num_choices
+        self.chosen_param_list = np.zeros(num_choices, dtype="int16")
         self.current = 0
+
+        self.net = net
+        self.num_params = sum(p.numel() for p in net.parameters())
+        self.last_module = list(net.modules())[-1]
 
     def activation_hook(self, start_index):
         def hook(module, input, output):
-            batch_mean = torch.mean(output, 0)
-            max_idx = torch.argmax(batch_mean)
-            weight = module.weight[max_idx]
-            _, indices = torch.sort(weight, descending=True)
-            indices = indices.detach().cpu().numpy() + start_index
-
-            if self.current + len(indices) > self.num_neurons:
-                self.activated_neurons[self.current :] = indices[
-                    : self.num_neurons - self.current
-                ]
-                self.current = self.num_neurons - 1
+            # Determine the number of neurons to be chosen
+            module_size = len(module.weight.view(-1))
+            if module is self.last_module:
+                quota = self.num_choices - self.current
             else:
-                self.activated_neurons[
-                    self.current : self.current + len(indices)
-                ] = indices
-                self.current += len(indices)
+                quota = int(self.num_choices * module_size / self.num_params)
+
+            if isinstance(module, nn.Linear):
+                # Compute the allotted number of neurons for each rows
+                num_weights_per_index = module.weight.size(1)
+                required_rows = quota // num_weights_per_index + 1
+
+                # Get list of indices of neurons with highest activation
+                batch_mean = torch.mean(output, 0)
+                index_list = torch.sort(batch_mean, descending=True)[1][:required_rows]
+
+                # Get the indices of the weights corresponding to the neurons
+                for index in index_list:
+                    weight = module.weight[index]
+                    _, indices = torch.sort(torch.abs(weight), descending=True)
+                    # Add the start position of the module when network is flattened.
+                    indices = (
+                        indices.detach().cpu().numpy()
+                        + start_index  # start of the module
+                        + num_weights_per_index * index.item()  # start of the index
+                    )
+
+                    num_saving = (
+                        quota - num_weights_per_index * (required_rows - 1)
+                        if index == index_list[-1]
+                        else num_weights_per_index
+                    )
+                    self.chosen_param_list[
+                        self.current : self.current + num_saving
+                    ] = indices[:num_saving]
+                    self.current += num_saving
+
+            elif isinstance(module, nn.Conv2d):
+                num_weights_per_kernel = (
+                    module.weight.size(1)
+                    * module.weight.size(2)
+                    * module.weight.size(3)
+                )
+                required_kernels = quota // num_weights_per_kernel + 1
+
+                batch_mean = torch.mean(output, (0, 2, 3))
+                index_list = torch.sort(batch_mean, descending=True)[1][
+                    :required_kernels
+                ]
+
+                # Get the indices of the weights corresponding to the neurons
+                for index in index_list:
+                    weight = module.weight[index].flatten()
+                    _, indices = torch.sort(torch.abs(weight), descending=True)
+                    # Add the start position of the module when network is flattened.
+                    indices = (
+                        indices.detach().cpu().numpy()
+                        + start_index  # start of the module
+                        + num_weights_per_kernel * index.item()  # start of the index
+                    )
+
+                    num_saving = (
+                        quota - num_weights_per_kernel * (required_kernels - 1)
+                        if index == index_list[-1]
+                        else num_weights_per_kernel
+                    )
+                    self.chosen_param_list[
+                        self.current : self.current + num_saving
+                    ] = indices[:num_saving]
+                    self.current += num_saving
 
         return hook
 
@@ -48,8 +105,8 @@ class TopNActivations(nn.Module):
             hook.remove()
 
     def initialize_neurons(self):
-        self.activated_neurons = np.zeros(self.num_neurons, dtype="int16")
+        self.chosen_param_list = np.zeros(self.num_choices, dtype="int16")
         self.current = 0
 
     def get_parameters(self):
-        return self.activated_neurons
+        return self.chosen_param_list
