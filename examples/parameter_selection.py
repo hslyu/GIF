1826f256352d4dfb529edc9dc8d3152e9ff41276
hslyu <hslyu4@gmail.com>
2023-07-10 01:52:55 +0900

    Update parameter selection

--- examples/parameter_selection.py
+++ examples/parameter_selection.py
@@ -51,6 +51,7 @@ def main():
         net = torch.nn.DataParallel(net)
         cudnn.benchmark = True
 
+    flatten = True
     net, train_epoch = load_net(
         net,
         "/home/hslyu/research/PIF/checkpoints/Figure_3/FullyConnectedNet/cross_entropy/ckpt_0.0.pth",
@@ -59,13 +60,16 @@ def main():
     # For just doing some exps
     # net = FullyConnectedNet(28 * 28, 600, 10, 200, 0.1).to(device)
     # train_epoch = 50
+    net = TinyNet().to(device)
+    train_epoch = 50
+    flatten = False
 
     net.eval()
-    flatten = True
     net_name = net.__class__.__name__
+    num_param = sum(p.numel() for p in net.parameters())
     print(
         f"==> Building {net_name} finished. "
-        + f"\n    Number of parameters: {sum(p.numel() for p in net.parameters())}"
+        + f"\n    Number of parameters: {num_param}"
     )
 
     criterion = nn.CrossEntropyLoss()
@@ -78,14 +82,14 @@ def main():
     data_loader = mnist.MNISTDataLoader(batch_size, num_workers, flatten=flatten)
     train_loader, val_loader, _ = data_loader.get_data_loaders()
 
-    print("==> Registering hooks..")
-    # Make hooks
-    net_parser = parameter_selection.TopNActivations(net, 600)
-    net_parser.register_hook()
-
     print("==> Computing total loss..")
     total_loss = forward(net, train_loader, criterion)
     print(total_loss)
+
+    print("==> Registering hooks..")
+    # Make hooks
+    net_parser = parameter_selection.TopNActivations(net, int(num_param * 0.5))
+    net_parser.register_hook()
 
     print("==> Computing influence..")
     # One batch of train data
@@ -93,7 +97,6 @@ def main():
         for batch_idx, (data, target) in enumerate(train_loader):
             print("batch_idx: ", batch_idx)
             net_parser.initialize_neurons()
-            loss = criterion(net(data[0:10].to(device)), target[0:10].to(device))
 
             idx = target == 8
             data = data[idx]
